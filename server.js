ee89b67305ea0d23d475c26017559fb2b89f6388
Philip Ziolkowski <philipz4848@gmail.com>
2022-07-25 23:00:36 -0700

    testing other routes and adding custom middle ware and morgan

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
+app.use(morgan('dev'))
 
-app.get('/', (req, res) => [
+const log = (req, res, next) => {
+    console.log('logging')
+    next()
+}
+app.use(log())
+
+let db = []
+
+
+app.get('/', log, (req, res) => [
     res.send({ message: 'Hello from Express'})
 ])
 
+app.get('/message', [log, log], (req, res) => {
+    res.send({db})
+})
+
+app.delete('/message', (req, res) => {
+    console.log('db before', db)
+    db = db.filter(m => m.text !== req.body.text)
+    console.log('db after', db)
+    res.send({ message: 'Successfully deleted'})
+})
+
 app.post('/', (req, res) => {
     console.log('request body: ', req.body)
-    res.send({ message: 'post back'})
+    db.push(req.body)
+    res.send({ message: 'message created'})
 })
 
 export const start = () => {
